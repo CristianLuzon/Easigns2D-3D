ecf1802de16e181c8bf9246c674ccad0b1bf05ce
CristianLuzon <cristianluzon@gmail.com>
2018-01-16 01:20:33 +0100

    improved easing transform script

--- Assets/Scripts/EasingTransform.cs
+++ Assets/Scripts/EasingTransform.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EasingTransform : MonoBehaviour
 {
@@ -26,24 +27,45 @@ public class EasingTransform : MonoBehaviour
     }
     public TEasingEnd m_EasingEnd;
 
+    public enum TEasingDelayType
+    {
+        NONE = 0,
+        FIXEDELAY,
+        RANDOMDELEY
+    }
+    public TEasingDelayType m_EasingDelayType;
+
+    public bool m_ChangeAlpha;
+    public bool m_ResetDelay;
+
     public Vector3 m_InitalValue;
     public Vector3 m_FinallValue;
     Vector3 m_DeltaValue;
 
-    public float m_CurrentTime;
+    Color m_ColorEasing;
+    Image m_Image;
+    public Vector2 m_AlphaValues;
+    public float m_AlphaDeltaValue;
+
+    float m_CurrentTime;
     public float m_EasingDuration;
 
     bool m_Restart;
     bool m_PingPong;
     bool m_TimeForward;
 
+    public float m_CurrentDeleyTime;
+    public float m_StarFixedtDelay;
+    public Vector2 m_RangeRandomDeley;
+
     void Start()
     {
         m_DeltaValue = m_FinallValue - m_InitalValue;
+        m_AlphaDeltaValue = m_AlphaValues.y - m_AlphaValues.x;
         m_CurrentTime = 0;
         m_TimeForward = false;
 
-        switch(m_EasingEnd)
+        switch (m_EasingEnd)
         {
             case TEasingEnd.NONE:
                 m_Restart = false;
@@ -60,23 +82,27 @@ public class EasingTransform : MonoBehaviour
             default:
                 break;
         }
+
+        if (m_EasingDelayType == TEasingDelayType.RANDOMDELEY)
+        {
+            m_StarFixedtDelay = Random.Range(m_RangeRandomDeley.x, m_RangeRandomDeley.y);
+        }
+
+        if (m_ChangeAlpha)
+        {
+            m_Image = GetComponent<Image>();
+            m_ColorEasing = m_Image.color;
+        }
     }
 
 	void Update()
     {
-        if(m_CurrentTime <= m_EasingDuration)
+        if(m_CurrentTime <= m_EasingDuration && m_CurrentDeleyTime >= m_StarFixedtDelay)
         {
-            if(!m_TimeForward) m_CurrentTime += Time.deltaTime;
-            else if(m_TimeForward) m_CurrentTime -= Time.deltaTime;
-
-            /*ejemplo
-             Vector3 l_EasingValue = new Vector3(Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
-                                                 Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
-                                                 Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-
-            transform.localPosition = l_EasingValue;*/
+            if (m_CurrentDeleyTime > m_StarFixedtDelay) m_CurrentDeleyTime = m_StarFixedtDelay;
 
             Vector3 l_EasingValue = new Vector3();
+            float l_EsingAlpha = 0f;
 
             switch(m_EasingType)
             {
@@ -84,46 +110,82 @@ public class EasingTransform : MonoBehaviour
                     l_EasingValue = new Vector3(Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                 Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                 Easing.ExpoEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.ExpoEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
                     break;
                 case TEasingType.CIRC:
                     l_EasingValue = new Vector3(Easing.CircEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                 Easing.CircEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                 Easing.CircEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-                   break;
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.CircEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
+                    break;
                case TEasingType.QUINT:
                    l_EasingValue = new Vector3(Easing.QuintEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                Easing.QuintEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                Easing.QuintEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-                   break;
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.QuintEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
+                    break;
                case TEasingType.QUART:
                    l_EasingValue = new Vector3(Easing.QuartEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                Easing.QuartEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                Easing.QuartEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-                   break;
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.QuartEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
+                    break;
                case TEasingType.QUAD:
                    l_EasingValue = new Vector3(Easing.QuadEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                Easing.QuadEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                Easing.QuadEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-                   break;
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.QuadEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
+                    break;
                case TEasingType.SINE:
                    l_EasingValue = new Vector3(Easing.SineEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                Easing.SineEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                Easing.SineEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
-                   break;
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.SineEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
+                    break;
                case TEasingType.BACK:
                    l_EasingValue = new Vector3(Easing.BackEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                Easing.BackEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                Easing.BackEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.BackEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
                     break;
                 case TEasingType.BOUNCE:
                     l_EasingValue = new Vector3(Easing.BounceEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                 Easing.BounceEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                 Easing.BounceEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.BounceEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
                     break;
                 case TEasingType.ELASTIC:
                     l_EasingValue = new Vector3(Easing.ElasticEaseInOut(m_CurrentTime, m_InitalValue.x, m_DeltaValue.x, m_EasingDuration),
                                                 Easing.ElasticEaseInOut(m_CurrentTime, m_InitalValue.y, m_DeltaValue.y, m_EasingDuration),
                                                 Easing.ElasticEaseInOut(m_CurrentTime, m_InitalValue.z, m_DeltaValue.z, m_EasingDuration));
+                    if (m_ChangeAlpha)
+                    {
+                        l_EsingAlpha = Easing.ElasticEaseInOut(m_CurrentTime, m_AlphaValues.x, m_AlphaValues.y, m_EasingDuration);
+                    }
                     break;
                 default:
                     break;
@@ -144,7 +206,18 @@ public class EasingTransform : MonoBehaviour
                     break;
             }
 
-            if(m_CurrentTime > m_EasingDuration)
+            if (m_ChangeAlpha)
+            {
+                m_ColorEasing.a = l_EsingAlpha;
+                m_Image.color = m_ColorEasing;
+            }
+
+            m_CurrentTime += Time.deltaTime;
+
+            Debug.Log("Easing: " + l_EsingAlpha);
+            Debug.Log("alpha: " + m_ColorEasing.a);
+
+            if (m_CurrentTime > m_EasingDuration)
             {
                 switch(m_TransformSelected)
                 {
@@ -161,23 +234,36 @@ public class EasingTransform : MonoBehaviour
                         break;
                 }
 
+                if (m_ChangeAlpha)
+                {
+                    m_ColorEasing.a = 1f;
+                    m_Image.color = m_ColorEasing;
+                }
+
                 if (m_Restart)
                 {
+                    if (m_ResetDelay) m_CurrentDeleyTime = 0;
                     m_CurrentTime = 0;
                 }
                 else if (m_PingPong)
                 {
+                    if (m_ResetDelay) m_CurrentDeleyTime = 0;
                     m_CurrentTime = 0;
                     Vector3 l_Initial = m_InitalValue;
                     m_InitalValue = m_FinallValue;
                     m_FinallValue = l_Initial;
                     m_DeltaValue = m_FinallValue - m_InitalValue;
+
+                    float l_AlphaInitial = m_AlphaValues.x;
+                    m_AlphaValues.x = m_AlphaValues.y;
+                    m_AlphaValues.y = l_AlphaInitial;
+                    m_AlphaDeltaValue = m_AlphaValues.y - m_AlphaValues.x;
                 }
             }
         }
         else
         {
-
+            m_CurrentDeleyTime += Time.deltaTime;
         }
     }
 }
